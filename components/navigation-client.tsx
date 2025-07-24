b27e55986494db432bf379cba71122d0cb3fbee0
sisoko13 <dnjstkatjrgh@gmail.com>
2025-07-24 22:38:24 +0900

    IST Lab을 네비게이션바 맨 왼쪽으로 이동

--- components/navigation-client.tsx
+++ components/navigation-client.tsx
@@ -63,7 +63,7 @@ export default function Navigation() {
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16 w-full">
           {/* Logo */}
-          <div className="flex items-center flex-shrink-0">
+          <div className="flex items-center">
             <button
               className="text-xl font-bold hover:text-blue-300 transition-colors focus:outline-none"
               onClick={() => router.push("/")}
@@ -73,7 +73,7 @@ export default function Navigation() {
           </div>
 
           {/* Desktop Menu */}
-          <div className="hidden md:flex items-center space-x-8 flex-1 justify-center">
+          <div className="hidden md:flex items-center space-x-8 flex-1 justify-end mr-8">
             {menuItems.map((item) => (
               <div
                 key={item.id}
@@ -94,7 +94,7 @@ export default function Navigation() {
           </div>
 
           {/* University Info */}
-          <div className="hidden md:block text-right flex-shrink-0">
+          <div className="hidden md:block text-right">
             <div className="text-sm">Kunsan National University</div>
             <div className="text-xs opacity-80">Software Department</div>
           </div>
